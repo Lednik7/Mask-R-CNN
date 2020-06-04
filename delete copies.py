047c56d623beb1734649d92d076933cf8eaf6c05
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-06-04 16:56:11 +0300

    Create delete copies.py

---
+++ delete copies.py
@@ -0,0 +1,30 @@
+#pip install ImageHash
+import imagehash as m
+import os
+
+# Sample/jpg/
+dir = input()
+
+ask = (input("Delete copies?: ")).replace(" ", "").lower() #Yes or No || 1 or 0
+
+path = os.path.join(os.getcwd(), dir)
+
+original = []
+copy = []
+
+try:
+    for filename in os.listdir(path):
+        path2 = os.path.join(path, filename)
+        hash = m.dhash(Image.open(path2))
+        if hash in original:
+            copy.append(filename)
+            if ask == "yes" or 1:
+                os.remove(path2)
+        else:
+            original.append(hash)
+        
+    print("Number of originals: " + str(len(original)))
+    print("Number of copies: " + str(len(copy)))
+            
+except FileNotFoundError:
+    print("Wrong way")
