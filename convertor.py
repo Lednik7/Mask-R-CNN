fb2a624b527687c7afaf4f8306f4a8a6bc5f0fcf
Lednik7 <52196169+Lednik7@users.noreply.github.com>
2020-06-04 12:27:36 +0300

    Create convertor.py

---
+++ convertor.py
@@ -0,0 +1,26 @@
+import os
+from PIL import Image
+name = input("Введите название новой папки: ")
+
+dir = input("Введите дерикторию: ")
+
+path = os.path.join(os.getcwd(), dir)
+
+path2 = os.path.join(path, name)
+
+try:
+    os.mkdir(path2)
+    print("Папка создана")
+except:
+    print("Папка уже создана")
+    pass
+             
+for filename in os.listdir(path):
+    if filename != name:
+        try:
+            img = Image.open(os.path.join(path, filename))
+            img.save(os.path.join(path2, filename + ".jpg"))
+        except PermissionError:
+            print("Ошибка. Возможно в каталоге есть папка: " + filename)
+
+print("Good")
